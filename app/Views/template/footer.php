3ca05399a908d70826f379059c159ab96387baf4
Jonnibones <js5800672@gmail.com>
2022-02-21 16:17:40 -0300

    t-max-test

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -1,6 +1,10 @@
         
         
         
-     
+     <footer style="height: 80px;" class=" bg-dark text-white footer fixed-bottom position-sticky">
+        <div class="container">
+            <span><p class="text-center">T-max-test <?php echo "<br>" .date('d-m-Y');?></p></span>
+        </div>
+     </footer>
     </body>
 </html>
